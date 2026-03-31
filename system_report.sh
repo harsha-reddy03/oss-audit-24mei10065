88cf479af45dcef0de0fa7eb917ac23424a9d523
R HARSHA REDDY <harshareddy0561@gmail.com>
2026-03-31 22:37:26 +0530

    Add system_report.sh for system identity reporting

---
+++ system_report.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: R Harsha Reddy | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="R Harsha Reddy"
+SOFTWARE_CHOICE="Linux"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+HOME_DIR=$HOME
+UPTIME=$(uptime -p)
+DATE_TIME=$(date)
+DISTRO=$(grep '^PRETTY_NAME' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+# --- Display ---
+echo "========================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "========================================"
+echo "Software        : $SOFTWARE_CHOICE"
+echo "Distribution    : $DISTRO"
+echo "Kernel Version  : $KERNEL"
+echo "User            : $USER_NAME"
+echo "Home Directory  : $HOME_DIR"
+echo "Uptime          : $UPTIME"
+echo "Date & Time     : $DATE_TIME"
+echo "----------------------------------------"
+echo "License Info    : Linux is distributed under the GNU General Public License (GPL)."
+echo "========================================"
+
